bedc3e5f381866b4b0e503a05c735b605a237a8c
Heshan Wanigasooriya <heshan@heidisoft.com>
2018-03-13 18:59:52 -0400

    first commit

---
+++ social_post.api.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ */
+
+function hook_social_post_accounts(){
+  return array(
+    'twitter' => array(
+      'name' => 'Twitter',
+      'settings' => array(
+        'consumer_key' => array(
+          '#type' => 'textfield',
+          '#title' => t('Consumer Key')
+        ),
+        'consumer_secret' => array(
+          '#type' => 'textfield',
+          '#title' => t('Consumer Secret')
+        )
+      ),
+      'authentication' => '_social_post_twitter_social_post_authentication_callback',
+      'post_authentication' => '_social_post_twitter_social_post_post_authentication_callback',
+      'publish' => '_social_post_twitter_social_post_publish_callback'
+    );
+  );
+}
